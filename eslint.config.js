e466b1c74a6d7a55cad3386d8facac25b2467ed6
curiouskarma <plgauntt@hotmail,com>
2026-01-05 21:29:06 -0600

    updated eslint config to ignore tests directory and saved results

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
 import js from "@eslint/js";
 import globals from "globals";
 import json from "@eslint/json";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  	globalIgnores([
+		"tests/*", // ignore its content
+	]),
 ]);
